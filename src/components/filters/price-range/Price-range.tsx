64d8824e24b9febdcb7364efedbda2f6b084d186
mirlan-mambetov <prefect.mirlan@proton.me>
2024-01-16 15:20:56 +0600

    fixed

--- src/components/filters/price-range/Price-range.tsx
+++ src/components/filters/price-range/Price-range.tsx
@@ -4,8 +4,10 @@ import { FC, useState } from "react"
 import style from "./price.range.module.scss"
 
 export const PriceRangeComponent: FC = () => {
-	const [value1, setValue1] = useState("")
-	const [value2, setValue2] = useState("")
+	const MAX_PRICE = 300000
+	const MIN_PRICE = 100
+	const [minPrice, setMinPrice] = useState(MIN_PRICE)
+	const [maxPrice, setMaxPrice] = useState(MAX_PRICE)
 	return (
 		<div className={style.wrap}>
 			<div className={style.range}>
@@ -13,26 +15,28 @@ export const PriceRangeComponent: FC = () => {
 					<span>Цена от</span>
 					<input
 						type="range"
-						min={100}
-						max={300000}
+						min={MIN_PRICE}
+						max={MAX_PRICE}
 						step={10}
-						onChange={(e) => setValue1(e.currentTarget.value)}
+						onChange={(e) => setMinPrice(+e.currentTarget.value)}
+						defaultValue={MIN_PRICE}
 					/>
 				</div>
-				<small>{value1} KGS</small>
+				<small>{minPrice} KGS</small>
 			</div>
 			<div className={style.range}>
 				<div className={style.value}>
 					<span>Цена до</span>
 					<input
 						type="range"
-						min={100}
-						max={300000}
+						min={MIN_PRICE}
+						max={MAX_PRICE}
+						defaultValue={MAX_PRICE}
 						step={10}
-						onChange={(e) => setValue2(e.currentTarget.value)}
+						onChange={(e) => setMaxPrice(+e.currentTarget.value)}
 					/>
 				</div>
-				<small>{value2} KGS</small>
+				<small>{maxPrice} KGS</small>
 			</div>
 		</div>
 	)
