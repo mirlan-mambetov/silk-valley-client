ebec8a8e710e16403395d26d48c1d842921843bc
mirlan-mambetov <prefect.mirlan@proton.me>
2024-01-29 14:40:03 +0600

    update

--- src/components/product/product-detail/mobile-detail-info/Mobile-detail-info.tsx
+++ src/components/product/product-detail/mobile-detail-info/Mobile-detail-info.tsx
@@ -1,6 +1,10 @@
 "use client"
 
-import { ButtonComponent, ProductPriceComponent } from "@/components"
+import {
+	ButtonComponent,
+	ProducAttributeComponent,
+	ProductPriceComponent,
+} from "@/components"
 import { IProduct } from "@/interfaces/product.interface"
 import Image from "next/image"
 import { FC } from "react"
@@ -19,17 +23,32 @@ export const MobileDetailInfoComponent: FC<IMobileDetailInfoComponentProps> = ({
 					<div className={style.image}>
 						<Image src={data.poster} alt={data.title} width={90} height={150} />
 					</div>
-					<div className={style.items}>
+					<div className={style.content}>
 						<div className={style.item}>
 							<h3 className={style.name}>{data.title}</h3>
-						</div>
-						<div className={style.item}>
 							<ProductPriceComponent
 								price={data.price}
 								className={style.price}
 								discount={data.discount}
 								orientation="column"
 								size="1xxl"
+							/>
+						</div>
+
+						<div className={style.item}>
+							<ProducAttributeComponent
+								className={style.attribute}
+								title="Цвета"
+								data={["Черный", "Белый", "Зеленый", "Черно-белый"]}
+								size="1xl"
+							/>
+						</div>
+						<div className={style.item}>
+							<ProducAttributeComponent
+								title="Размеры"
+								className={style.attribute}
+								data={["32x", "34x", "36x", "38x"]}
+								size="1xl"
 							/>
 						</div>
 					</div>
