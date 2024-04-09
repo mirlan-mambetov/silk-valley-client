6b326a1f9e96113ee09a7355d09f6e4ec18777e0
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-04-09 23:07:18 +0600

    add animation loader

--- src/components/cart/cart-range/Cart-range.tsx
+++ src/components/cart/cart-range/Cart-range.tsx
@@ -4,7 +4,7 @@ import { ButtonComponent } from "@/components"
 import { useStoreActions } from "@/hooks/store/useStoreActions"
 import { IProduct } from "@/interfaces/product.interface"
 import cn from "classnames"
-import { DetailsHTMLAttributes, FC } from "react"
+import { DetailsHTMLAttributes, FC, useState } from "react"
 import { LuMinus, LuPlus } from "react-icons/lu"
 import style from "./cart-range.module.scss"
 
@@ -22,20 +22,50 @@ export const CartRangeComponent: FC<ICartRangeComponentProps> = ({
 	...props
 }) => {
 	const { changedQuantity } = useStoreActions()
+	const [minusLoading, setMinusLoading] = useState(false)
+	const [plusLoading, setPlusLoading] = useState(false)
+
+	const changeMinusHandler = async () => {
+		setMinusLoading(true)
+		await new Promise<void>((resolve) => {
+			setTimeout(() => {
+				changedQuantity({ type: "minus", id: product.id })
+				resolve()
+			}, 1300)
+		})
+		setMinusLoading(false)
+	}
+	const changePlusHandler = async () => {
+		setPlusLoading(true)
+		await new Promise<void>((resolve) => {
+			setTimeout(() => {
+				changedQuantity({ type: "plus", id: product.id })
+				resolve()
+			}, 1300)
+		})
+		setPlusLoading(false)
+	}
+
 	return (
 		<div className={cn(style.quantity, className)} {...props}>
 			{text && <small>Количество</small>}
 			<div className={style.quantity_item}>
 				<ButtonComponent
+					disabled={minusLoading}
+					isLoading={minusLoading}
 					title="Убрать"
-					onClick={() => changedQuantity({ type: "minus", id: product.id })}
+					btnType="rangeButton"
+					onClick={changeMinusHandler}
 				>
 					<LuMinus />
 				</ButtonComponent>
 				<span>{quantity}</span>
 				<ButtonComponent
+					disabled={plusLoading}
+					isLoading={plusLoading}
 					title="Добавить"
-					onClick={() => changedQuantity({ type: "plus", id: product.id })}
+					btnType="rangeButton"
+					onClick={changePlusHandler}
 				>
 					<LuPlus />
 				</ButtonComponent>
