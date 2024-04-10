d166bdde4bf4b83ba8f8ef4e6933acb8178bb8a9
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-04-10 10:00:48 +0600

    changed

--- src/api/config/api-config.ts
+++ src/api/config/api-config.ts
@@ -1,1 +1,3 @@
-export const APP_URI = process.env.NEXT_PUBLIC_API_BASE_HOST
+export const APP_URI =
+	process.env.NEXT_PUBLIC_API_BASE_HOST ||
+	"https://9aa4-94-143-197-34.ngrok-free.app/api/v1"
--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -6,9 +6,12 @@ import { IProduct } from "@/interfaces/product.interface"
 export const revalidate = 3600
 
 // FETCH ALL PRODUCTS
-export async function fetchProduct(): Promise<IProduct[]> {
+export async function fetchProducts(): Promise<IProduct[]> {
 	try {
 		const response = await fetch(`${APP_URI}/product`)
+		if (!response.ok) {
+			throw new Error("Fetch failed for products..")
+		}
 		return response.json()
 	} catch (err) {
 		throw new Error(String(err))
@@ -16,7 +19,7 @@ export async function fetchProduct(): Promise<IProduct[]> {
 }
 
 export default async function HomePage() {
-	const products = await fetchProduct()
+	const products = await fetchProducts()
 	return (
 		<>
 			<section>
